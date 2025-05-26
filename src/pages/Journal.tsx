3d91f237a6c253503076a1e6ee18e9947324bbf0
nasifuad <nasifuad007@gmail.com>
2025-05-26 10:37:19 +0600

    yeee

---
+++ src/pages/Journal.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Journal = () => {
+  return (
+    <>
+      <p>Journal</p>
+      <Outlet />z
+    </>
+  );
+};
+
+export default Journal;
---
+++ src/router/route.tsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout/Layout";
+import Home from "../pages/Home";
+import Journal from "../pages/Journal";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/journal",
+        element: <Journal />,
+        children: [
+          {
+            path: "/journal/:id",
+            element: <div>Journal 1 2 3</div>,
+          },
+        ],
+      },
+      {
+        path: "/profile",
+        element: <div>Profile</div>,
+      },
+    ],
+  },
+]);
+
+export default router;
